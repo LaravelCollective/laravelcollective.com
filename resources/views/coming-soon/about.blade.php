789761ff12e4aec3ad664599ad939733521461cd
Adam Engebretson <adam@enge.me>
2015-02-05 10:13:11 -0600

    Fixing Typo
    Signed-off-by: Adam Engebretson <adam@enge.me>

--- resources/views/coming-soon/about.blade.php
+++ resources/views/coming-soon/about.blade.php
@@ -7,7 +7,7 @@
     </header>
 
     <div class="body wrapper-768">
-      <p>The Laravel Collective is a Laravel community organization dedicated to helping maintain core Laravel components that have been deprecated by the framework. Our main maintancne will include component updates as needed for various Laravel version updates.</p>
+      <p>The Laravel Collective is a Laravel community organization dedicated to helping maintain core Laravel components that have been deprecated by the framework. Our main maintenance will include component updates as needed for various Laravel version updates.</p>
       <p>We strive to help the Laravel community flourish and stay strong. With the understanding that Laravel grows incredibly quickly, our goal is to make sure that people can continue to grow with Laravel without having to re-write deprecated (and sometimes necessary) components.</p>
 
     </div>
