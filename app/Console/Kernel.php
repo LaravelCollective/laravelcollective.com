54798e00b6495d483c992be9883cdae9170a99e1
Adam Engebretson <adam@enge.me>
2015-02-11 16:04:23 -0600

    Increasing doc update frequency
    Signed-off-by: Adam Engebretson <adam@enge.me>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,6 @@ class Kernel extends ConsoleKernel {
   protected function schedule(Schedule $schedule)
   {
     $schedule->command('docs:refresh')
-      ->hourly();
+      ->everyFiveMinutes();
   }
 }
