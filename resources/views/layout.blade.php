d27a124a51a9cf6d0245ed233888859383b7f34a
Adam Engebretson <adam@enge.me>
2015-02-16 10:03:14 -0600

    Adding GoSquared

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,5 +15,13 @@
 
     <script src="/js/app.js"></script>
     @yield('scripts')
+    <script>
+      !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+        arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+        d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+          insertBefore(d,q)}(window,document,'script','_gs');
+
+      _gs('GSN-057185-B');
+    </script>
   </body>
 </html>
