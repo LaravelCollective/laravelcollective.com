b270a48916bb2163085648c2c15f0531bbd1e892
Tom Shafer <tj@tjshafer.com>
2015-12-04 18:27:19 -0700

    Google font url fix
    Signed-off-by: Tom Shafer <tj@tjshafer.com>

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
   |
   */
 
-  'url'             => 'http://laravelcreative.com',
+  'url'             => 'http://laravelcollective.com',
 
   /*
   |--------------------------------------------------------------------------
